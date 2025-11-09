f63c2ea3c3e0efa96f920298036565d2e0fe0045
pcsatpals <pcsatpals@gmail.com>
2025-11-09 11:04:29 +0530

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriberSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // one who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // one to whom subscriber is subscribing
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Subscription = mongoose.model("SubScription", subscriberSchema);
