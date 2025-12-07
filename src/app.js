a4804bf47f9006d79f50257ace1d3007aba1deb2
pcsatpals <pcsatpals@gmail.com>
2025-12-07 20:57:12 +0530

    Added dashboard queries

--- src/app.js
+++ src/app.js
@@ -28,12 +28,14 @@ import videoRouter from "./routes/video.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/like", likeRouter);
 app.use("/api/v1/comment", commentRouter);
